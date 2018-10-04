648a6c96305b1420b8f76507d5362cb60f883db0
Jonatan <s185096@student.dtu.dk>
2018-10-04 11:32:10 +0200

    Jonatans commit

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -8,7 +8,7 @@ public class Dice {
 		int d3=Math.round(d2);             // 0-5 integer
 		return d3 + 1;                     // 1-6		
 	}
-	
+	// Dette er min, Jonatans kommentar.
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
 		for (int i=1; i<n; i++) {
