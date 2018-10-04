1e5c205bd28354b917a0f15190576713eb95c8de
aleksanderlj <aleksanderlj@hotmail.com>
2018-10-04 11:46:24 +0200

    DiceTest fil lavet

---
+++ src/terning/DiceTest.java
@@ -0,0 +1,14 @@
+package terning;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @org.junit.jupiter.api.Test
+    void roll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void rollMultiple() {
+    }
+}
